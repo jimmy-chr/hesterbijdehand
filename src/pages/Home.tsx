98c83ea5280e7c2d24d7f6873cedc3e97f05bf73
jimmy-chr <jimmy_chr@hotmail.com>
2021-12-05 15:39:26 +0100

    adding home tags

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Container, Grid, Image, Header, Divider } from "semantic-ui-react";
+import { Grid, Image, Header, Divider } from "semantic-ui-react";
 import ImageCarousel from "../components/ImageCarousel";
 
 const Home = () => {
@@ -14,27 +14,8 @@ const Home = () => {
             <Image src="https://cataas.com/cat" />
           </Grid.Column>
           <Grid.Column width={12}>
-            <Header as="h2">Welkom</Header>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed
-              risus ultricies tristique nulla aliquet enim tortor at. Sagittis
-              id consectetur purus ut faucibus pulvinar elementum. Accumsan
-              tortor posuere ac ut consequat semper viverra nam libero. Lacus
-              viverra vitae congue eu. Sagittis id consectetur purus ut faucibus
-              pulvinar elementum integer enim.
-            </p>
-            <p>
-              Aliquam sem fringilla ut morbi tincidunt augue interdum velit.
-              Tellus molestie nunc non blandit massa. Vulputate mi sit amet
-              mauris commodo quis imperdiet massa tincidunt. Tortor posuere ac
-              ut consequat semper viverra nam. Lectus vestibulum mattis
-              ullamcorper velit sed ullamcorper morbi tincidunt. Eget nulla
-              facilisi etiam dignissim. Aliquet risus feugiat in ante metus.
-              Hendrerit dolor magna eget est. Nibh cras pulvinar mattis nunc sed
-              blandit. Senectus et netus et malesuada fames ac turpis. Quis
-              ipsum suspendisse ultrices gravida.
-            </p>
+            <Header as="h2">{t("home.header-title")}</Header>
+            {t("home.header-description")}
           </Grid.Column>
         </Grid.Row>
         <Divider />
